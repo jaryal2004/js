32980e40b7e5a0489d5157ca27f528de73a824b5
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-07-15 20:01:39 +0000

    declaring variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,21 @@
+const accountId=567 //cant be changed
+let accountEmail="buuihiuh"
+var accountPass="8787"
+accountCity="jaipur"
+
+
+// accountId=7 //not allowed
+// var {} scope initial js dont work with scopes
+
+
+
+
+
+accountEmail="gyugyu"
+accountPass="8543"
+accountCity="begaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
+
+
+
