217134dd89135b50969afbdfcb2cf6fb84090e3b
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-07-16 17:37:28 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js code as newer version js
+
+// alert("3+4") //we are uisng nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("Hitesh")
+
+let name="hitesh" //string=>""
+let age=18  //number=> 2^53 range, bigInt
+let islog= false  //boolean=>true or false
+let state=null
+//null=> standalone value
+//undefined=> value id not defined of some declared variable
+//symbol=>unique
+
+//object
+console.log(typeof "hitesh");
+
+//type of null is object
+// type of undefined is undefined
+
+
