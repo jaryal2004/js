4e6b43d4d60635f53fb611d6cd6e3b1128303fcb
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-07-16 18:21:59 +0000

    comparisons

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+//true
+
+console.log("2">1);
+//true
+
+
+
+//avoid these type of conversion
+console.log(null>0)
+//false
+//null==0 false
+//null>=0 true
+
+console.log(undefined ==0)
+// false
+
+//strict check ===
+console.log("2"===2)
+// false
