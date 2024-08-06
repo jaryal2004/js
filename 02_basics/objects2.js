0bb3adee2fd57f1458e04d5600a24c93259c8d4d
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-08-06 12:39:33 +0000

    json

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -48,3 +48,40 @@ console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+//destructuring
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+//course.courseInstructor
+const {courseInstructor:instructor}=course//extract the value from object
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// {} destructuring
+// const navbar= ({company}) => {
+// }
+// navbar(company="hitesh")
+
+
+
+//API:whatever data we want to fetch 
+//json format,object
+// {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+//array format
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
