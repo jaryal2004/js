df3cfc912bfec0141a2ea7797deb59a001c19e2a
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-08-28 12:31:17 +0000

    javascript execution phase

---
+++ 03_basics/Jsexec.js
@@ -0,0 +1,11 @@
+//Javascript execution context
+//{code}-> global execution context->this(current context)
+//function execution context
+//eval execution context
+//{}->creation phase (only memory allocation)-> execution phase
+// 1. global->this->memory phase->execution phase
+//global execution also get deleted once executed
+
+
+//call stack
+//lifo
