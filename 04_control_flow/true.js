2189346e2109bb03b4b3d70122acab108ea1fa23
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-08-30 12:47:24 +0000

    truth

---
+++ 04_control_flow/true.js
@@ -0,0 +1,27 @@
+const userEmail="h@gmail.com"
+if(userEmail){
+    console.log("got email");
+}
+else{
+    console.log("not having email");
+}
+
+//false values 
+//false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+//true values
+//"0",'false'," ",[],{},function(){} empty function
+
+
+//to check array is empty or not
+// if(userEmail.length===0){
+//     console.log("empty array");
+// }
+
+//object is empty or not
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("empty object");
+
+// }
+
