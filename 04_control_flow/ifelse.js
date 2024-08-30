65d2cbdcd686d9e207eb22b44c1fbf1690fbb25d
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-08-30 12:32:22 +0000

    control statements

---
+++ 04_control_flow/ifelse.js
@@ -0,0 +1,32 @@
+//if
+
+// if(condition){  //condition should be true for code execution
+//     //scope
+// }
+
+// const isUserLoggedIn=true;
+// if(2=="2"){
+//        console.log("executed");
+// }
+
+//if(){}else{}
+
+//  const score=200
+//  if(score>100){
+//     const power ="fly"
+//     console.log(`user power :${power}`);
+//  }
+ //var is global
+//  console.log(`user power :${power}`);
+
+
+//short hand notation
+// const balance=1500;
+// if(balance<500) console.log("test"),
+// console.log("test2");
+
+// if(){}
+// else if(){}
+//else {}
+
+
