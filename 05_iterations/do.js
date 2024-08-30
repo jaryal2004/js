a892db92d295a26b4f474412f532d5ca86337e18
Payal <157163674+jaryal2004@users.noreply.github.com>
2024-08-30 14:56:56 +0000

    for loop

---
+++ 05_iterations/do.js
@@ -0,0 +1,20 @@
+let i=0;
+while(i<=10){
+    console.log(`value of index is ${i}`);
+    i+=2;
+}
+
+let myArray=['flash','batman','superman']
+let arr=0;
+while(arr<myArray.length){
+    console.log(`value is 4{myArray[arr]}`);
+    arr+=1;
+}
+
+//do while
+let score=11;
+do{
+console.log(`score is ${score}`);
+score++;
+}
+while(score<=10);
---
+++ 05_iterations/one.js
@@ -0,0 +1,11 @@
+//for loop
+for(let i=0;i<10;i++){
+    const element=i;
+    console.log(element);
+}
+
+let myArray=["flash","batman","superman"]
+for(let i=0;i<myArray.length;i++){
+    const element=myArray[i];
+    console.log(element);
+}
